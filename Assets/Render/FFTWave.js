d015fc37860075f8febe21b3bbe48888e413d12a
Wolf <wolferacing@gmail.com>
2019-10-10 22:52:24 -0400

    no message

---
+++ Assets/Render/FFTWave.js
@@ -0,0 +1,86 @@
+﻿#pragma strict
+
+	public var material     : Material;
+
+	
+	var freqData = 8192;
+	private var bufferPoints : ComputeBuffer;
+	private var bufferPos    : ComputeBuffer;
+	
+	private var pos      : Vector3[];
+	private var samples  :   float[];	
+
+	
+	function Start () {
+		Audition ();
+	}
+	
+	function Audition () {
+
+		//make verts
+		var verts = [ Vector3(0,.1,0), Vector3(0,-.1,0), Vector3(0,.1,0) ] ;
+		
+		//make complex array
+		
+		var pos = new Vector3[ freqData ];
+		var samples = new float[ freqData ];
+		for ( var i = 0; i < freqData; ++i)
+		{
+			samples[i]=Mathf.Sin(i/100.0);
+			pos[i] = Vector3
+			( Random.value*2 , Random.value*2, Random.value*2 );
+		}
+		
+		ReleaseBuffers ();
+		
+		bufferPoints = new ComputeBuffer (3, 12);
+		bufferPoints.SetData (verts);
+		material.SetBuffer ("buf_Points", bufferPoints);
+
+		bufferPos = new ComputeBuffer (freqData, 12);
+		bufferPos.SetData (pos);
+		material.SetBuffer ("buf_Positions", bufferPos);
+		
+	}
+	
+
+	function OnPostRender () {
+		if(Time.time<2){material.SetPass (0);
+		Graphics.DrawProcedural (MeshTopology.LineStrip, 3, freqData);}
+		
+	}
+	
+	function OnDisable() {
+		ReleaseBuffers ();
+	}
+	
+	function ReleaseBuffers() {
+		if (bufferPoints != null) bufferPoints.Release();
+		bufferPoints = null;
+		if (bufferPos != null) bufferPos.Release();
+		bufferPos = null;
+	}	
+	
+		
+	function Update () {
+
+		material.SetInt("offset", 0);
+		material.SetInt("skipn", 0);
+		material.SetInt("sbpm", 0);
+		material.SetInt("sbpmoffset", 0);
+		
+
+Audition ();
+	}
+
+
+	
+	function OnGUI() {
+        // GUI.Label(new Rect(Screen.width/2, 20  , 200, 20), "VYNIL ");
+        // GUI.Label(new Rect(5, 100, 170, 700), 
+		// "R tag\nY fix\nU span\nI tap\nUp bpm\nDown bpm\nmouse zoom\nP load list\n"+ sbpm +"\n"+songname 
+		// );		
+    }
+
+
+
---
+++ Assets/Render/fftshader.shader
@@ -0,0 +1,46 @@
+﻿Shader "DX11/fftshader" {
+SubShader {
+Pass {
+
+CGPROGRAM
+#pragma target 5.0
+
+#pragma vertex vert
+#pragma fragment frag
+
+#include "UnityCG.cginc"
+
+StructuredBuffer<float3> buf_Points;
+StructuredBuffer<float3> buf_Positions;
+
+struct ps_input {
+	float4 pos : SV_POSITION;
+	float4 col : COLOR;
+};
+
+ps_input vert (uint id : SV_VertexID , uint inst : SV_InstanceID)
+{
+	ps_input o;
+	
+	float3 worldPos = 
+	buf_Points[id]  *  buf_Positions[inst].y  *  10 
+	+ float3 (   buf_Positions[inst].x ,  buf_Positions[inst].y-4  , 0);
+	
+	o.pos = mul (UNITY_MATRIX_VP, float4(worldPos,1.0f));
+	o.col = float4(sin(inst/1700)/4+.75,sin(inst/2300)/4+.75,sin(inst/7100)/4+.75,1);
+	return o;
+}
+
+float4 frag (ps_input i) : COLOR
+{
+	//return float4(1,0.5f,0.0f,1);
+	return i.col;//
+}
+
+ENDCG
+
+}
+}
+
+Fallback Off
+}
